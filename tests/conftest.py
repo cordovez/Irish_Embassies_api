cbf89019d66edb9304be87e28389761ce50624b6
Juan Carlos Cordovez-Mantilla <cordovez@el-cordovez.com>
2024-07-22 19:02:43 +0200

    Update: Countries testing
    I can't get pytest to start a temporary local mongodb to test the beanie Document classes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,19 @@
-import sys
-import os
+import pytest
+from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+from mongodb.models import CountryDocument
 
-# Add the parent directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+@pytest.fixture(scope="module")
+async def test_db():
+    client = AsyncIOMotorClient("mongodb://localhost:27017")
+    db = client.test_db  # Create a test database
+    await init_beanie(database=db, document_models=[CountryDocument])
+
+    yield db
+    client.close()
+
+
+@pytest.fixture(autouse=True)
+def setup_test_db(test_db):
+    pass
