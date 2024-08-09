187917cc4c3fa030d5fd57357bc4be0c71507c8d
Juan Carlos Cordovez-Mantilla <cordovez@el-cordovez.com>
2024-08-09 08:30:40 +0200

    Update: imports in representation.py
    models where incorrectly referenced.

--- schemas/representation.py
+++ schemas/representation.py
@@ -1,6 +1,6 @@
 import pydantic
-from models.contact import ContactDetails
-from models.diplomat import DiplomatModel
+from mongodb.contact import ContactDetails
+from mongodb.models import DiplomatDocument
 from typing import Optional
 from typing import Literal
 
@@ -8,5 +8,5 @@ from typing import Literal
 class RepresentationModel(pydantic.BaseModel):
     type_of: Literal["representation"]
     representation: str
-    head_of_mission: Optional[DiplomatModel] | None
+    head_of_mission: Optional[DiplomatDocument] | None
     contact: Optional[ContactDetails] | None
